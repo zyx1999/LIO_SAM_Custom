69dcbe7588f8066dbcc987bb678ea1602c08877c
YuxuanZhao <18218490078@163.com>
2024-02-23 11:50:49 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yuxuanzhao/ros_workspace/liosam_ws/src/LIO-SAM
+# Build directory: /home/yuxuanzhao/ros_workspace/liosam_ws/src/LIO-SAM/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/lio_sam-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(lio_sam_MESSAGE_FILES "/home/yuxuanzhao/ros_workspace/liosam_ws/src/LIO-SAM/msg/cloud_info.msg")
+set(lio_sam_SERVICE_FILES "/home/yuxuanzhao/ros_workspace/liosam_ws/src/LIO-SAM/srv/save_map.srv")
